2ec79e4443a49cd4a9fa82392c85857098e84d07
Dmitry Galinsky <dima.exe@gmail.com>
2014-02-10 09:14:19 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -2,51 +2,15 @@
 
 set -e
 
-DOCKER="docker"
-
-if [ -z $PREFIX ] ; then
-  PREFIX=dmexe
-fi
-
-if [ -z $CONTAINER ] ; then
-  echo "* Build SSH image"
-  ${DOCKER} build -t $PREFIX/vexor-precise:latest .
-
-  CONTAINER=$(${DOCKER} run -d $PREFIX/vexor-precise)
-  echo "* Spawn container ${CONTAINER}"
-fi
-
-if [ -z $IMAGE_TAG ] ; then
-  IMAGE_TAG=latest
-fi
-
-if [ -z $IMAGE_NAME ] ; then
-  IMAGE_NAME=$PREFIX/vexor-precise-full
-fi
-
-ADDR=$(${DOCKER} inspect ${CONTAINER} | grep IPAddress | cut -d '"' -f 4)
-echo "* Using container IPAddress ${ADDR}"
-
-ANSIBLE_HOSTS=$(pwd)/.ansible_hosts
-echo $ADDR > $ANSIBLE_HOSTS
-export ANSIBLE_HOSTS
-export ANSIBLE_HOST_KEY_CHECKING=False
-export ANSIBLE_SSH_ARGS="-o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-
-if [ ! -z $TAGS ] ; then
-  TAGS=" --tags ${TAGS}"
-fi
-
-ANSIBLE_OPTS="--ask-pass -u vexor -s ${TAGS}"
-
-echo "* Run playbooks, type 'vexor' in 'SSH password:' prompt"
-(cd playbooks && ansible-playbook site.yml $ANSIBLE_OPTS)
-
-echo "* Kill container ${CONTAINER}"
-$DOCKER kill $CONTAINER
-
-echo "* Commit container ${CONTAINER} to ${IMAGE_NAME}:${IMAGE_TAG}"
-$DOCKER commit ${CONTAINER} ${IMAGE_NAME}:${IMAGE_TAG}
-
-echo "* DONE"
-
+case $1 in
+  "precise")
+    packer build packer/precise.json
+    ;;
+  "precise-full")
+    packer build packer/precise-full.json
+    ;;
+  *)
+    echo "Usage $0 (precise|precise-full)"
+    exit 1
+    ;;
+esac
