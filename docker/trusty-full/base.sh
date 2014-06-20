d9b05b20a500380abbadac38e081127c06d93be4
Dmitry Galinsky <dima.exe@gmail.com>
2014-06-20 04:25:58 +0400

    fixed redis server startup

--- docker/trusty-full/base.sh
+++ docker/trusty-full/base.sh
@@ -9,6 +9,7 @@ export PYTHONUNBUFFERED=1
 
 cd /tmp/provision
 
+apt-get update
 apt-get install -qy software-properties-common
 apt-add-repository -y ppa:rquillo/ansible
 apt-get update
@@ -16,7 +17,7 @@ apt-get install -qy ansible python-apt
 
 ansible-playbook playbooks/site.yml -v -i playbooks/inventory -c local
 
-apt-get purge -qy ansible
+apt-get purge -qy ansible python-apt
 apt-get autoremove -qy
 apt-get clean
 
