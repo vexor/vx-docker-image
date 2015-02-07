c0b06a61f79c0add6ecddbf1c0a52308f7fe6338
Dmitry Galinsky <dima.exe@gmail.com>
2015-02-07 19:58:01 +0300

    update scripts

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ VERSION=$(cat version.txt)
 
 case $1 in
   push)
-    echo docker push vexor/trusty:${VERSION}
-    docker push vexor/trusty:${VERSION}
+    echo docker push quay.io/dmexe/trusty:${VERSION}
+    docker push quay.io/dmexe/trusty:${VERSION}
     ;;
   *)
     cd docker/trusty
-    echo docker build -t vexor/trusty:${VERSION} .
-    docker build --rm --no-cache -t vexor/trusty:${VERSION} .
-    docker run -it vexor/trusty:${VERSION} /sbin/my_init -- /bin/bash /var/vexor/tests.sh
+    echo docker build -t quay.io/dmexe/trusty:${VERSION} .
+    docker build --rm --no-cache -t quay.io/dmexe/trusty:${VERSION} .
+    docker run -it quay.io/dmexe/trusty:${VERSION} /sbin/my_init -- /bin/bash /var/vexor/tests.sh
     ;;
 esac
